9aa04a31f4e6a49f554e94f30e5c3f7d6b59695c
Akshay kumar <akshaymundlagiri@gmail.com>
2025-05-06 14:03:52 +0530

    Update studentController.js

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -1,1 +1,22 @@
+const Student = require('../models/studentModel');
+
+const getStudents = async (req, res) => {
+  const students = await Student.find();
+  res.json(students);
+};
+
+const addStudent = async (req, res) => {
+  const { name } = req.body;
+  const student = new Student({ name });
+  await student.save();
+  res.status(201).json(student);
+};
+
+const deleteStudent = async (req, res) => {
+  const { id } = req.params;
+  await Student.findByIdAndDelete(id);
+  res.status(204).end();
+};
+
+module.exports = { getStudents, addStudent, deleteStudent };
 
